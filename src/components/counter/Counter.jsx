71c5606041741bcce3d722825e27dc667da0ecf6
Altaf <altaf@groupbima.com>
2019-11-03 12:28:36 +0530

    directly calling parent component from child component

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -46,33 +46,33 @@ class Counter extends Component {
 class CounterButton extends Component {
     constructor() {
         super();
-        this.state = {
-            counter: 0
-        }
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
+        // this.state = {
+        //     counter: 0
+        // }
+        // this.increment = this.increment.bind(this);
+        // this.decrement = this.decrement.bind(this);
     }
     render() {
         return (
             <div className="CounterButton">
-                <button onClick={this.increment}>+{this.props.by}</button>
-                <button onClick={this.decrement}>-{this.props.by}</button>
+                <button onClick={() => { return this.props.incrementMethod(this.props.by) }}>+{this.props.by}</button>
+                <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
                 {/* <span className="count">{this.state.counter}</span> */}
             </div>
         );
     }
-    increment() {
-        this.setState({
-            counter: this.state.counter + this.props.by
-        });
-        this.props.incrementMethod(this.props.by)
-    }
-    decrement() {
-        this.setState(prevState => {
-            return { counter: this.state.counter - this.props.by }
-        });
-        this.props.decrementMethod(this.props.by)
-    }
+    // increment() {
+    //     this.setState({
+    //         counter: this.state.counter + this.props.by
+    //     });
+    //     this.props.incrementMethod(this.props.by)
+    // }
+    // decrement() {
+    //     this.setState(prevState => {
+    //         return { counter: this.state.counter - this.props.by }
+    //     });
+    //     this.props.decrementMethod(this.props.by)
+    // }
 }
 
 CounterButton.defaultProps = {
