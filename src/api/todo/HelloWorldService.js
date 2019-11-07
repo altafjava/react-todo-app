444b816127c9bb9d94ea0f51fc8aedc9b39ac289
Altaf <samaltaf6@gmail.com>
2019-11-07 22:32:04 +0530

    integrating react to spring boot api

---
+++ src/api/todo/HelloWorldService.js
@@ -0,0 +1,8 @@
+import Axios from "axios";
+
+class HelloWorldService {
+    executeHelloWorldService() {
+        return Axios.get('http://localhost:8080/hello-world')
+    }
+}
+export default new HelloWorldService()
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import HelloWorldService from '../../api/todo/HelloWorldService.js'
 
 class WelcomeComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.state = {
+            welcomeMessage: ''
+        }
+    }
     render() {
         return (
             <>
                 <h1>Welcome!</h1>
-                <div className="container">Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link></div>
+                <div className="container">
+                    Welcome {this.props.match.params.name}.
+                    You can manage your todos <Link to="/todos">here</Link>
+                </div>
+                <div className="container">
+                    Click here to get customized message.
+                    <button className="btn btn-success" onClick={this.retrieveWelcomeMessage}>Get Welcome Message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
+                </div>
             </>
+        )
+    }
+    retrieveWelcomeMessage() {
+        HelloWorldService.executeHelloWorldService()
+            .then(response => this.handleSuccessfulResponse(response))
+    }
+    handleSuccessfulResponse(response) {
+        this.setState(
+            { welcomeMessage: response.data }
         )
     }
 }
